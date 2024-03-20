afec1dc06d9c8ef642d9341aa3eecbd073572529
Krishna <krishnasharma78957@gmail.com>
2024-03-20 20:02:51 +0530

    first commit

---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Logo({width = '100px'}) {
+  return (
+    <div>
+      <img src="https://ideogram.ai/api/images/direct/hUuyGTDtRKaFhSfLaU_pHA.png" alt="logo" width={width} className='rounded-full hover:shadow-xl hover:shadow-slate-900 hover:duration-300'   />
+    </div>
+  )
+}
+
+export default Logo
